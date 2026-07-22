44ec64fdfae859c1bcd3ffc2101104aae545c786
Pablo Andrade <andradepablooo18@gmail.com>
2026-07-22 00:05:20 -0500

    Made my first implmentation/test of my own library! I made a cube and then various functions to move it, scale it with mouse wheel, rotate it by pressing left button and a basic rotation animation

--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
 #include "core/Engine.h"
-#include "graphics/Camera.h"
-#include "graphics/Mesh.h"
-#include "scene/Object3D.h"
+#include "core/types.h"
+#include "input/Input.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 bool setup(void);
-void update(Engine* e);
+void update(Input* input, f32 dt);
 void draw(Engine* e);
 void destroy(void);
-
-Camera* camera = NULL;
-Mesh* cube_mesh = NULL;
-Object3D* cube = NULL;
 
 int main(void) {
     Engine* e = NULL;
@@ -25,6 +20,7 @@ int main(void) {
     }
 
     EngineCallbacks callbacks = {.update = update, .draw = draw};
+
     Engine_run(e, &callbacks);
 
     Engine_destroy(&e);
@@ -34,26 +30,10 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-bool setup(void) {
-    if (!Camera_create(&camera)) {
-        return false;
-    }
-    if (!Mesh_create_cube(&cube_mesh)) {
-        return false;
-    }
-    if (!Object3D_create(&cube)) {
-        return false;
-    }
-    Object3D_set_mesh(cube, cube_mesh);
-    return true;
-}
+bool setup(void) { return true; }
 
-void update(Engine* e) {}
+void update(Input* input, f32 dt) {}
 
-void draw(Engine* e) { Engine_draw_object3D(e, camera, cube); }
+void draw(Engine* e) {}
 
-void destroy(void) {
-    Camera_destroy(&camera);
-    Mesh_destroy(&cube_mesh);
-    Object3D_destroy(&cube);
-}
+void destroy(void) {}
---
+++ src/tests/cube.c
@@ -0,0 +1,122 @@
+#include "core/Engine.h"
+#include "core/types.h"
+#include "graphics/Camera.h"
+#include "graphics/Mesh.h"
+#include "input/Input.h"
+#include "input/keys.h"
+#include "math/common.h"
+#include "scene/Object3D.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SPEED 10.0f
+#define ZOOM_SPEED 2.0f
+#define SENSIVITY 0.1f
+#define ROTATION_SPEED HALF_PI
+
+bool setup(void);
+void update(Input* input, f32 dt);
+void draw(Engine* e);
+void destroy(void);
+
+void move_cube(Input* input, f32 dt, f32 velocity);
+void zoom_cube_with_mouse_wheel(Input* input, f32 dt, f32 zoom_speed);
+void rotate_cube_when_pressed(Input* input, f32 dt, f32 sensivity);
+void rotate_cube_animation(f32 dt, f32 rotation_speed);
+
+Camera* camera = NULL;
+Mesh* cube_mesh = NULL;
+Object3D* cube = NULL;
+
+int main(void) {
+    Engine* e = NULL;
+    if (!Engine_create(&e)) {
+        return EXIT_FAILURE;
+    }
+
+    if (!setup()) {
+        return EXIT_FAILURE;
+    }
+
+    EngineCallbacks callbacks = {.update = update, .draw = draw};
+
+    Engine_run(e, &callbacks);
+
+    Engine_destroy(&e);
+
+    destroy();
+
+    return EXIT_SUCCESS;
+}
+
+bool setup(void) {
+    if (!Camera_create(&camera)) {
+        return false;
+    }
+    if (!Mesh_create_cube(&cube_mesh)) {
+        return false;
+    }
+    if (!Object3D_create(&cube)) {
+        return false;
+    }
+    Object3D_set_mesh(cube, cube_mesh);
+    Object3D_set_position(cube, (Vector3){0.0f, 0.0f, -10.0f});
+    return true;
+}
+
+void update(Input* input, f32 dt) {
+    move_cube(input, dt, SPEED);
+    zoom_cube_with_mouse_wheel(input, dt, ZOOM_SPEED);
+    rotate_cube_when_pressed(input, dt, SENSIVITY);
+    // rotate_cube_animation(dt, ROTATION_SPEED);
+}
+
+void move_cube(Input* input, f32 dt, f32 speed) {
+    f32 dx = 0.0f;
+    f32 dy = 0.0f;
+    if (Input_is_key_down(input, KEY_W)) {
+        dy = 1;
+    }
+    if (Input_is_key_down(input, KEY_S)) {
+        dy = -1;
+    }
+    if (Input_is_key_down(input, KEY_A)) {
+        dx = -1;
+    }
+    if (Input_is_key_down(input, KEY_D)) {
+        dx = 1;
+    }
+    if (dx != 0.0f && dy != 0.0f) {
+        dx *= 0.70710678f; // 1 / sqrt(2)
+        dy *= 0.70710678f;
+    }
+    Object3D_translate(cube, (Vector3){dt * dx * speed, dt * dy * speed, 0.0f});
+}
+
+void zoom_cube_with_mouse_wheel(Input* input, f32 dt, f32 zoom_speed) {
+    int wheel_y = Input_get_wheel_y(input);
+    Object3D_scale(cube, (Vector3){dt * wheel_y * zoom_speed,
+                                   dt * wheel_y * zoom_speed,
+                                   dt * wheel_y * zoom_speed});
+}
+
+void rotate_cube_when_pressed(Input* input, f32 dt, f32 sensivity) {
+    if (Input_is_mouse_button_down(input, MOUSE_BUTTON_LEFT)) {
+        Vector2 delta = Input_get_mouse_delta_position(input);
+        Object3D_rotate(cube, (Vector3){dt * delta.y * sensivity,
+                                        dt * delta.x * sensivity, 0.0f});
+    }
+}
+
+void rotate_cube_animation(f32 dt, f32 rotation_speed) {
+    Object3D_rotate(cube,
+                    (Vector3){dt * rotation_speed, dt * rotation_speed, 0.0f});
+}
+
+void draw(Engine* e) { Engine_draw_object3D(e, camera, cube); }
+
+void destroy(void) {
+    Camera_destroy(&camera);
+    Mesh_destroy(&cube_mesh);
+    Object3D_destroy(&cube);
+}
